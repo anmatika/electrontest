dcf1b9e8caead6a0a7f74e1d860175e24a3276f5
anmatika <4ntti.matikainen@gmail.com>
2017-05-28 08:03:32 +0300

    working grid example

--- app/components/Poloniex/OpenOrders.js
+++ app/components/Poloniex/OpenOrders.js
@@ -1,19 +1,51 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import ReactDataGrid from 'react-data-grid';
 import { Link } from 'react-router-dom';
 
 const OpenOrders = ({ state, showOpenOrdersAsync }) => {
     function onClick(e) {
         showOpenOrdersAsync();
     }
-    function prune () {
+    function prune() {
     //     return `${state.openOrders[0].key}: amount: ${state.openOrders[0].values[0].amount}`;
         const keys = Object.keys(state.OpenOrders);
         return state.OpenOrders;
     }
 
-    return (
+    const columns = [
+      { key: 'id', name: 'ID' },
+      { key: 'title', name: 'Title' },
+      { key: 'count', name: 'Count' }];
+
+  function createRows() {
+        const rows = [];
+        for (let i = 1; i < 1000; i++) {
+        rows.push({
+            id: i,
+            title: `Title ${i}`,
+            count: i * 1000
+        });
+        }
+
+    return rows;
+  }
+
+  const rows = createRows();
+  function rowGetter(i) {
+      return rows[i];
+  }
+
+  return (
     <div>
           OpenOrders
+          <ReactDataGrid
+            columns={columns}
+            rowGetter={rowGetter}
+            rowsCount={rows.length}
+            minHeight={500}
+          />
+
           <textarea value={'foo'} />
           <button onClick={onClick}>Show OpenOrders</button>
 
